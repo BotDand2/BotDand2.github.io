4d384cc3f0c21d4b8a5e70e66cc90ca476f16f46
BotDand2 <botdand2@qq.com>
2022-08-09 15:36:35 +0800

    Site updated: 2022-08-09 15:36:34

--- js/chocolate.js
+++ js/chocolate.js
@@ -54,3 +54,15 @@ $(function() {
     // 调用气泡方法
     bubble();
 })
+
+
+var mobile_sidebar_menus = document.getElementById("sidebar-menus");
+if (mobile_sidebar_menus) {
+    var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+        "menus_item_child"
+    );
+    var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+    for (var i = 0; i < menus_item_child.length; i++) {
+        menus_item_child[i].style.display = "none";
+    }
+}
